5067a0ee6315964b78b4835bc4eda054af479248
Violettanik <Violettanik27@yandex.ru>
2024-05-17 18:00:28 -0400

    Add ansible

---
+++ init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE email_and_id ( id INT PRIMARY KEY, email VARCHAR(255) );
+CREATE TABLE phonenumber_and_id ( id INT PRIMARY KEY, phonenumber VARCHAR(255) );
+INSERT INTO email_and_id (id, email) VALUES (1, 'Violettanik27@yandex.ru'), (2, 'Violettanik27062005@yandex.ru');
+INSERT INTO phonenumber_and_id (id, phonenumber) VALUES (1, '89858782912'), (2, '89169329062'); 
+
